b087f94e9a4f90545a31b1c399dd6ef9d5928c87
Raymond Loman <raymond@carpago.nl>
2022-05-09 11:55:29 +0200

    Improve structure and add some comments

--- src/nl/duo/tennis/inheritance/sub/Butterfly.java
+++ src/nl/duo/tennis/inheritance/sub/Butterfly.java
@@ -9,8 +9,11 @@ public class Butterfly extends Animal {
     private Dog d = new Dog("Wodan");
 
     public Butterfly() {
-        this.animalOnly();
-//        a.animalOnly(); // vout
-//        d.animalOnly(); // vout
+        this.animalOnly(); // prima, want immers this (de Butterfly) is een animal
+//        a.animalOnly(); // vout want: dit is gewoon een instance field en geen inheritance structuur
+//                  je zou verwachten wellicht dat dit werkt en hier testen ze met het examen ook op
+//                  maar je moet het zo zien alsof je een public static void main maakt met een variable er in 
+//                  dan werkt het toch ook niet???
+//        d.animalOnly(); // vout: zelfsde als vorige; a(nimal) of d(dog) doet er niet toe
     }
 }

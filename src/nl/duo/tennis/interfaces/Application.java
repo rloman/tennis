82d1fcddfc5fed80a52a767b209878f062b7716f
Raymond Loman <raymond@carpago.nl>
2022-05-10 11:40:45 +0200

    Finish interfaces

--- src/nl/duo/tennis/interfaces/Application.java
+++ src/nl/duo/tennis/interfaces/Application.java
@@ -4,5 +4,18 @@ public class Application {
 
     // Maak hier een TV en zet hem aan, zet op kanaal 3 en zet uit.
 
+    public static void main(String[] args) {
+        Samsung s = new Samsung();
 
+        s.on();
+        s.switchChannel(3);
+        s.off();
+
+        // Mooier is ...
+        Television tv = new Samsung();
+
+        tv.on();
+        tv.switchChannel(5);
+        tv.off();
+    }
 }

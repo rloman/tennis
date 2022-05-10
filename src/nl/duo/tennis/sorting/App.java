d65a4660162c27690cdee987bb90e059858fe817
Raymond Loman <raymond@carpago.nl>
2022-05-10 22:23:14 +0200

    Add demo code for Sorting

--- src/nl/duo/tennis/sorting/App.java
+++ src/nl/duo/tennis/sorting/App.java
@@ -1,6 +1,9 @@
 package nl.duo.tennis.sorting;
 
+import java.net.SocketTimeoutException;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class App {
 
@@ -12,5 +15,11 @@ public class App {
         System.out.println(Arrays.toString(numbers));
 
         System.out.println(Arrays.toString(numbers));
+
+        List<String> names = Arrays.asList("Jeroen", "Willem", "Jaap");
+        System.out.println(names);
+        Collections.sort(names);
+        System.out.println(names);
+
     }
 }
